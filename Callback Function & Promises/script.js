2c1f57be5d331053b6b67a6ae0d16a24e89037f8
Vrutesh Mayekar <vruteshpm02@gmail.com>
2024-02-06 21:30:48 +0530

    added event bubbling & Callback Function

---
+++ Callback Function & Promises/script.js
@@ -0,0 +1,32 @@
+// Callback Concept
+
+let container = document.querySelector(".container");
+
+const anonymous = () => {
+  console.log("Hello I am Anonymous Function");
+};
+
+const callback = (anonymous) => {
+  console.log("I am Callback 1");
+};
+
+function addPara(ptext, heading, callback) {
+  let para = document.createElement("p");
+  para.innerHTML = ptext;
+  container.appendChild(para);
+
+  let para2 = document.createElement("h1");
+  para2.innerText = heading;
+  container.appendChild(para2);
+}
+
+addPara(
+  "<h1>Hello I am Vrutesh</h1>",
+  "Frontend Developer",
+  callback(anonymous())
+);
+
+
+// Promises Concept
+
+
