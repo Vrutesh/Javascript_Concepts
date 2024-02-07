51e6ecdd9762870f7e3f5079ed64bc94bddc6ffc
Vrutesh Mayekar <vruteshpm02@gmail.com>
2024-02-07 12:15:50 +0530

    completed promises concept

--- Callback Function & Promises/promise.js
+++ Callback Function & Promises/promise.js
@@ -1,24 +1,45 @@
 // promise concept
 
-let promise1 = new Promise((resolve, reject)=>{
-    let randomNum = Math.random();
+let promise1 = new Promise((resolve, reject) => {
+    let randomNum = Math.random()
+    console.log(randomNum)
     if(randomNum < 0.5){
-        reject("Promise is rejected")
+        reject("System under Maintenance")
     }
     else{
         setTimeout(() => {
-            console.log("Vrutesh")
-            resolve("hello")
+           console.log("Resolved")
+           resolve("Hello")
         }, 1000);
-        
     }
-    console.log(randomNum)
 })
 
-promise1.then((randomNumber)=>{
-    console.log(randomNumber)
+let promise2 = new Promise((resolve, reject) => {
+    let randomNum = Math.random()
+    console.log(randomNum)
+    if(randomNum < 0.5){
+        reject("System under Maintenance 2")
+    }
+    else{
+        setTimeout(() => {
+           console.log("Resolved 2")
+           resolve("Hello 2")
+        }, 1000);
+    }
+})
+
+// promise1.then((Num)=>{
+//     console.log("hello")
+// }).catch((err)=>{
+//    console.log(err)
+// })
+
+
+let p3 = Promise.all([promise1, promise2])
+p3.then((randomNum)=>{
+    console.log(randomNum)
 }).catch((err)=>{
-    console.log("System Under Maintenance") 
+    console.log(err)
 })
 
 // console.log(promise1)
